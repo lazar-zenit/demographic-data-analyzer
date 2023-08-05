442fcbca229d09220c22620a935b9991d3a5f4bd
lazar-zenit <62837269+lazar-zenit@users.noreply.github.com>
2023-08-05 17:23:44 +0200

    update
    just couple of variables left

--- demographic_data_analyser_SOLUTION.py
+++ demographic_data_analyser_SOLUTION.py
@@ -9,19 +9,63 @@ race_count=df.groupby(['race']).size() #group by race and deliver size of that d
 average_age_men=df.loc[df['sex']=='Male'][['age']].mean().round(decimals=1) #
 
 
-percentage_of_bachelors=(df.loc[df['education']=='Bachelors', ['age']] 
-                         .count()#hack is to use another column to serve as an index so count will force single series
-                         /number_of_people*100).round(decimals=1) #calculate percentadn round it. beware of parentheses
-
-#higher education means 'Bachelors', 'Masters' and 'Doctorate'
+'''
 number_bachelors=df.loc[df['education']=='Bachelors', ['age']].count() #same hack, easier on syntax if done separately
 number_masters=df.loc[df['education']=='Masters', ['age']].count()
 number_doctorate=df.loc[df['education']=='Doctorate', ['age']].count()
+'''
 
-education=df.value_counts('education')
-print(education)
-     
+number_bachelors=(df['education'].value_counts())['Bachelors']
+number_masters=(df['education'].value_counts())['Masters']
+number_doctorate=(df['education'].value_counts())['Doctorate']
+
+percentage_of_bachelors=(number_bachelors/number_of_people*100).round(decimals=1)
+
 higher_education=number_bachelors+number_masters+number_doctorate #sum of higher degrees
 lower_education=number_of_people-higher_education #difference between all people and ones with higher degrees
 
+education_vs_salary=df.loc[:,['education','salary']] #just to make it easier on dislexy
+
+#BEWARE BRACKETS, EDUCATION CONDITION SOULD BE INCASED IN ITS OWN ROUND BRACKETS
+
+higher_education_rich=len(education_vs_salary[
+                              ((education_vs_salary['education'] == 'Bachelors') | 
+                              (education_vs_salary['education'] == 'Masters') | 
+                              (education_vs_salary['education'] == 'Doctorate')
+                              )
+                              & (education_vs_salary['salary']=='>50K')
+                              ]
+          )
+#for some reason
+lower_education_rich = len(education_vs_salary[
+                              ((education_vs_salary['education'] == '10th') | 
+                              (education_vs_salary['education'] == '11th') | 
+                              (education_vs_salary['education'] == '12th') |
+                              (education_vs_salary['education'] == '1st-4th') |
+                              (education_vs_salary['education'] == '5th-6th') |
+                              (education_vs_salary['education'] == '7th-8th') |
+                              (education_vs_salary['education'] == '9th') |
+                              (education_vs_salary['education'] == 'Assoc-acdm') |
+                              (education_vs_salary['education'] == 'Assoc-voc') |
+                              (education_vs_salary['education'] == 'HS-grad') |
+                              (education_vs_salary['education'] == 'Masters') |
+                              (education_vs_salary['education'] == 'Preschool') |
+                              (education_vs_salary['education'] == 'Prof-school') |
+                              (education_vs_salary['education'] == 'Some-college')
+                              )
+                              & (education_vs_salary['salary']=='>50K')
+                              ]
+                          )     
+
+
+#JUST FOR CHECKS
+salary=df.loc[:,['salary']].value_counts()
+
+min_work_hours=df['hours-per-week'].min()
+
+num_min_workers=len(df[
+    (df['hours-per-week']==min_work_hours)
+    & (df['salary']=='>50K')]
+    )
+rich_percentage=round(((len(df[df['salary']=='>50K']))/number_of_people*100), 1)#round has to be on the start because of the error "'float' object has no attribute 'round'
 
